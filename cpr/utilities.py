f71e1423e531469b9b9404e95350a2902f16cb48
Peter Beyersdorf <peter.beyersdorf@sjsu.edu>
2025-02-25 19:36:01 -0800

    Update utilities.py

--- cpr/utilities.py
+++ cpr/utilities.py
@@ -2527,15 +2527,26 @@ def exportGrades(assignment=None, fileName=None, delimiter=",", display=False, s
 							line+=str(student.pointsByCriteria[assignment.id][cid]) + delimiter
 						else:
 							line+="" + delimiter
-					line+=(str(points['creation']) + delimiter + 
-						str(points['curvedCreation']) + delimiter + 
-						str(points['review']) + delimiter + 
-						str(points['total']) + delimiter + 
-						str(points['curvedTotal']) + delimiter + 
-						'"' + student.creationComments[assignment.id] + '"' + delimiter +
-						'"' + student.reviewComments[assignment.id] + '"' + delimiter +
-						'"' + student.gradingExplanation + '"' + delimiter +
-						'"' + student.reviewGradeExplanation + '"')
+					try:
+						line+=(str(points['creation']) + delimiter + 
+							str(points['curvedCreation']) + delimiter + 
+							str(points['review']) + delimiter + 
+							str(points['total']) + delimiter + 
+							str(points['curvedTotal']) + delimiter + 
+							'"' + student.creationComments[assignment.id] + '"' + delimiter +
+							'"' + student.reviewComments[assignment.id] + '"' + delimiter +
+							'"' + student.gradingExplanation + '"' + delimiter +
+							'"' + student.reviewGradeExplanation + '"')
+					except:
+						print("Unable to export a grade for " + student.name)
+						line+=( delimiter + 
+							delimiter + 
+							delimiter +  
+							delimiter +  
+							delimiter + 
+							delimiter +
+							delimiter +
+							delimiter )				
 			else:
 				line+= ','
 			line+='\n'
