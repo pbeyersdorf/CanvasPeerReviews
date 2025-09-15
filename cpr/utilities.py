74f0fd263b548613d9d3f7032489d32d18b26cd5
Peter Beyersdorf <peter.beyersdorf@sjsu.edu>
2025-09-15 15:40:47 -0700

    tweaked the code for assigning peer reviews in lines 731-740 to try to avoid assigning self-reviews

--- cpr/utilities.py
+++ cpr/utilities.py
@@ -728,15 +728,16 @@ def assignPeerReviews(creationsToConsider, reviewers="randomize", numberOfReview
 				peer_review=assignAndRecordPeerReview(creation,reviewer, msg, secondPass)
 	# now that all creations have been assigned the target number of reviews, keep assigning until all students have the target number of reviews assigned
 	sortedReviewers=sorted(reviewers, key=lambda x: x.sortable_name) #sort alphabetically by last name
-	for reviewer in sortedReviewers:
+	sortedAvailableReviewers=[reviewer for reviewer in sortedReviewers if (not reviewer.assignedReviewOfCreation(creation)) and reviewer.id != creation.user_id and (reviewer.section == studentsById[creation.user_id].section or ignoreSections)]
+
+	for reviewer in sortedAvailableReviewers:
 		tic=time.time()
 		print(f"{reviewer.name} was assigned {reviewer.numberOfReviewsAssignedOnAssignment(creationsToConsider[0].assignment_id)} reviews")
 		while (reviewer.numberOfReviewsAssignedOnAssignment(creationsToConsider[0].assignment_id)  < params.numberOfReviews and reviewer.numberOfReviewsAssignedOnAssignment(creationsToConsider[0].assignment_id) < len(creationsToConsider)-1 and time.time()-tic < 1):
 			creation=random.choice(creationsToConsider)
-			if (reviewer.section == studentsById[creation.user_id].section or ignoreSections):
-				msg="---"
-				peer_review=assignAndRecordPeerReview(creation,reviewer, msg, secondPass)
-				#print("assigning " + str(reviewer.name)	 + " to review " + str(studentsById[creation.author_id].name) + "'s creation")			
+			msg="---"
+			peer_review=assignAndRecordPeerReview(creation,reviewer, msg, secondPass)
+			#print("assigning " + str(reviewer.name)	 + " to review " + str(studentsById[creation.author_id].name) + "'s creation")			
 	if len(graders)==0:
 		return
 	# finally assign to graders
